6c29d631f634c3d62e9d791cd4d91712dd2f7064
Joyce Staunton <jstaunto@ox-v6.21-school.ru>
2019-10-03 00:52:22 +0300

    little gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jstaunto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 20:42:19 by jstaunto          #+#    #+#             */
-/*   Updated: 2019/10/02 00:34:42 by jstaunto         ###   ########.fr       */
+/*   Updated: 2019/10/03 00:30:50 by jstaunto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 int			get_next_line(const int fd, char **line)
 {
-	char		buf[BUFF_SIZE];
-	int		c;
+	char		buf[BUFF_SIZE + 1];
+	int		c;  
 	int		i;
 	static char	*new;
 
-	if (!(read(fd, &buf, BUFF_SIZE - 1)))
-		return (1);
-	while ()	
-		new = ft_strjoin(new, buf);
+	if (!(read(fd, &buf, BUFF_SIZE)))
+		return (-1);
+	buf[BUFF_SIZE] = '\0';
+	*line = ft_strdup(buf);
+	
 	if (close(fd) == -1)
-		return (1);
-	ft_putstr(new);
+		return (-1);
 	return (0);
 }
 
@@ -34,7 +34,8 @@ int	main()
 	char	*line;
 	int	fd;
 
+
 	fd = open("42", O_RDONLY);
 	get_next_line(fd, &line);
-
+	ft_putstr(line);
 }
