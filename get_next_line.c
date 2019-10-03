52bb8c0ca4d71814af1d99a34efac12f97708039
Joyce Staunton <jstaunto@ox-v6.21-school.ru>
2019-10-04 00:35:21 +0300

    next com

--- get_next_line.c
+++ get_next_line.c
@@ -6,36 +6,42 @@
 /*   By: jstaunto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 20:42:19 by jstaunto          #+#    #+#             */
-/*   Updated: 2019/10/03 00:30:50 by jstaunto         ###   ########.fr       */
+/*   Updated: 2019/10/04 00:07:02 by jstaunto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+
+
 int			get_next_line(const int fd, char **line)
 {
 	char		buf[BUFF_SIZE + 1];
-	int		c;  
-	int		i;
-	static char	*new;
+	int		*nl;
+	char		*nwl;
+	static char	*rem;
+	int		rd;
 
-	if (!(read(fd, &buf, BUFF_SIZE)))
-		return (-1);
-	buf[BUFF_SIZE] = '\0';
-	*line = ft_strdup(buf);
-	
-	if (close(fd) == -1)
-		return (-1);
+	*line = ft_strnew(0);
+	while ((rd = read(fd, buf, BUFF_SIZE)))
+	{
+		if ((nl = ft_strchr(*line, '\n')))
+			break;
+		*nl = '\0';
+		*line = ft_strjoin(*line, buf);
+	}
 	return (0);
 }
 
-int	main()
+int			main()
 {
-	char	*line;
-	int	fd;
-
+	char		*line;
+	int		fd;
 
 	fd = open("42", O_RDONLY);
+	get_next_line(fd, &line);
+	ft_putstr(line);
+	ft_putchar('\n');
 	get_next_line(fd, &line);
 	ft_putstr(line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: jstaunto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 16:57:14 by jstaunto          #+#    #+#             */
-/*   Updated: 2019/10/01 22:19:02 by jstaunto         ###   ########.fr       */
+/*   Updated: 2019/10/03 21:54:06 by jstaunto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GNL_H
 # define GNL_H
 
-# define BUFF_SIZE 4
+# define BUFF_SIZE 5
 # include <unistd.h>
 # include <fcntl.h>
 # include <sys/types.h>
